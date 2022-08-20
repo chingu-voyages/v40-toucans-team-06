b2e7b0a856df11eff3835f3d4a3cf2ea6b6ebcef
Fatolu Stephen <smjssp@gmail.com>
2022-08-20 14:15:41 +0100

    Display cards dynamically

--- src/app.js
+++ src/app.js
@@ -1,2 +1,32 @@
 // Main file of the app
-import { products } from './data.js';
+import { products } from "./data.js";
+
+products.forEach(displayCards);
+console.log(products);
+function displayCards(product) {
+  console.log(product);
+
+  const cards = document.getElementById("card-container");
+  const {
+    image,
+    sizes: [xlarge, large, medium, small],
+    price,
+    color,
+  } = product || {};
+
+  const createCard = document.createElement("div");
+  createCard.classList.add("cards");
+  createCard.innerHTML = `
+  <img src=${image} alt="" srcset="" />
+          <div class="card-details">
+            <h6>T-shirt</h6>
+            <p>${color}</p>
+            <h6>${price} $</h6>
+            <a href="" class="btn">Buy Now</a>
+          </div>
+  `;
+  //   console.log(product);
+	  cards.appendChild(createCard);
+}
+
+// displayCards();
--- src/data.js
+++ src/data.js
@@ -1,116 +1,66 @@
 // This is the file where we'll have our mock data of T-Shirts.
 
 const products = [
-    {
-        image: "/src/Images/T-shirts/2.png",
-        sizes: [
-            "XL",
-            "L",
-            "M",
-            "S"
-        ],
-        price: 20,
-        color: "lime",
-    },
-    {
-        image: "/src/Images/T-shirts/4.png",
-        sizes: [
-            "XL",
-            "L",
-            "M",
-            "S"
-        ],
-        price: 20,
-        color: "red",
-    },
-    {
-        image: "/src/Images/T-shirts/6.png",
-        sizes: [
-            "XL",
-            "L",
-            "M",
-            "S"
-        ],
-        price: 20,
-        color: "blue",
-    },
-    {
-        image: "/src/Images/T-shirts/8.png",
-        sizes: [
-            "XL",
-            "L",
-            "M",
-            "S"
-        ],
-        price: 20,
-        color: "yellow",
-    },
-    {
-        image: "/src/Images/T-shirts/10.png",
-        sizes: [
-            "XL",
-            "L",
-            "M",
-            "S"
-        ],
-        price: 20,
-        color: "white",
-    },
-    {
-        image: "/src/Images/T-shirts/12.png",
-        sizes: [
-            "XL",
-            "L",
-            "M",
-            "S"
-        ],
-        price: 20,
-        color: "orange",
-    },
-    {
-        image: "/src/Images/T-shirts/14.png",
-        sizes: [
-            "XL",
-            "L",
-            "M",
-            "S"
-        ],
-        price: 20,
-        color: "pink"
-    },
-    {
-        image: "/src/Images/T-shirts/16.png",
-        sizes: [
-            "XL",
-            "L",
-            "M",
-            "S"
-        ],
-        price: 20,
-        color: "magenta",
-    },
-    {
-        image: "/src/Images/T-shirts/18.png",
-        sizes: [
-            "XL",
-            "L",
-            "M",
-            "S"
-        ],
-        price: 20,
-        color: "black",
-    },
-    {
-        image: "/src/Images/T-shirts/20.png",
-        sizes: [
-            "XL",
-            "L",
-            "M",
-            "S"
-        ],
-        price: 20,
-        color: "turquoise",
-    },
+  {
+    image: "./Images/Tshirts/2.png",
+    sizes: ["XL", "L", "M", "S"],
+    price: 20,
+    color: "lime",
+  },
+  {
+    image: "./Images/Tshirts//4.png",
+    sizes: ["XL", "L", "M", "S"],
+    price: 20,
+    color: "red",
+  },
+  {
+    image: "./Images/Tshirts/6.png",
+    sizes: ["XL", "L", "M", "S"],
+    price: 20,
+    color: "blue",
+  },
+  {
+    image: "./Images/Tshirts/8.png",
+    sizes: ["XL", "L", "M", "S"],
+    price: 20,
+    color: "yellow",
+  },
+  {
+    image: "./Images/Tshirts/10.png",
+    sizes: ["XL", "L", "M", "S"],
+    price: 20,
+    color: "white",
+  },
+  {
+    image: "./Images/Tshirts/12.png",
+    sizes: ["XL", "L", "M", "S"],
+    price: 20,
+    color: "orange",
+  },
+  {
+    image: "./Images/Tshirts/14.png",
+    sizes: ["XL", "L", "M", "S"],
+    price: 20,
+    color: "pink",
+  },
+  {
+    image: "./Images/Tshirts/16.png",
+    sizes: ["XL", "L", "M", "S"],
+    price: 20,
+    color: "magenta",
+  },
+  {
+    image: "./Images/Tshirts/18.png",
+    sizes: ["XL", "L", "M", "S"],
+    price: 20,
+    color: "black",
+  },
+  {
+    image: "./Images/Tshirts/20.png",
+    sizes: ["XL", "L", "M", "S"],
+    price: 20,
+    color: "turquoise",
+  },
 ];
 
 export { products };
