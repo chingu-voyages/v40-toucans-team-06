762a784a93e27935945d76cd2b88f1247d743451
amba05 <emma.amba@gmail.com>
2022-08-22 02:10:39 +0100

    Add: Search Functionality

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 // Main file of the app
 import { products } from "./data.js";
 
+//Declared variables to target nodes
 const payButtons = document.getElementsByClassName("button");
 const itemCounter = document.getElementById("item-counter");
+const cards = document.getElementById("card-container");
+
+
 let counterNumber = 0;
 itemCounter.innerHTML = counterNumber;
 
 products.forEach(displayCards);
+
+// Dynamically displays cards
 function displayCards(product) {
-  const cards = document.getElementById("card-container");
   const {
     image,
     sizes: [xlarge, large, medium, small],
@@ -31,6 +36,8 @@ function displayCards(product) {
   cards.appendChild(createCard);
 }
 
+
+// Item count in cart
 function increaseCounter() {
   counterNumber++;
   itemCounter.innerHTML = counterNumber;
@@ -38,4 +45,30 @@ function increaseCounter() {
 
 for (let i = 0; i < payButtons.length; i++) {
     payButtons[i].addEventListener("click", increaseCounter)
+}
+
+
+// search functionality
+document.addEventListener('submit', searchItem);
+
+function searchItem (e) {
+  e.preventDefault();
+
+  cards.innerHTML = '';
+  let item = e.target[0].value.toLowerCase();
+  let pattern = new RegExp(item);
+
+  let filtered = products.filter(elem => {
+    if (pattern.test(elem.title.toLowerCase())) {
+      return elem;
+    }
+  });
+
+  if (filtered.length == 0) {
+    cards.innerHTML = `<h1>Not Found</h1>`;
+  }
+
+  else {
+    filtered.forEach(displayCards);
+  }
 }
